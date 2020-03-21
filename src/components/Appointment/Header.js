61dbc6f76673825df8c0fe799b023acdf04f00d1
Hatem <hatem.deux@gmail.com>
2020-03-21 17:09:07 -0400

    Fixes and lints minor typos

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.scss';
 
-export default function Header (props) {
+export default function Header(props) {
   return (
   <header className="appointment__time">
     <h4 className="text--semi-bold">{props.time}</h4>
@@ -9,4 +9,4 @@ export default function Header (props) {
   </header>
 
   )
-}
+};
