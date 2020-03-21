452c65d4fc67643ace4161e0af36c02e16b0896c
Hatem <hatem.deux@gmail.com>
2020-03-20 23:42:10 -0400

    adds conditional CSS & correct 'sports remaining' message

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./DayListItem.scss";
+import classNames from "classnames";
 /* 
 name: String the name of the day
 spots: Number the number of spots remaining
@@ -14,10 +16,28 @@ The <h3> should display the spots remaining for a day
 
 
 export default function DayListItem(props) {
+
+  const  dayClass = classNames(
+    { "day-list__item": props }, 
+    { "day-list__item--selected": props.selected },
+    { "day-list__item--full": (props.spots === 0) }
+  );
+  
+  const formatSpots = function(spots){
+    let message = "";
+    if (spots === 0) {
+        message = `no spots remaining`;
+    } else if (spots === 1) {
+      message = `${spots} spot remaining`;
+    } 
+    else message = `${spots} spots remaining`;
+    return message;
+  };
+ 
   return (
-    <li onClick={() => props.setDay(props.name)}>      
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>      
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
