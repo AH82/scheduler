65a4066deed755b747e13cdf9dd87b94eac3a7f1
Hatem <hatem.deux@gmail.com>
2020-03-21 00:47:58 -0400

    DayList component implemented

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,9 +1,28 @@
 import React from "react";
 // import "./DayList.scss";
+import DayListItem from "components/DayListItem.js";
 import classNames from "classnames";
 
 export default function DayList(props) {
+
+  const { days } = props;
+
   return (
-    <ul></ul>
+  <ul>
+  {
+  days.map((day, index) => {
+    return (
+      <DayListItem
+        key={index}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  />
+        )
+      }
+    )
+  }
+  </ul>
   )
+      
 };
