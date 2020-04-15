2ab1f43eb0e96089099c215efe67ab57af9e649a
Hatem <hatem.deux@gmail.com>
2020-04-15 01:18:35 -0400

    continues implementation: buggy & awaiting assit

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,14 +4,19 @@ import InterviewerList from "../InterviewerList.js";
 
 
 export default function Form(props) {
-  const { name, setName } = useState();
-  const { interviewer, setInterviewer } = useState();
+  const { name, setName } = useState(props.name || "");
+  const { interviewer, setInterviewer } = useState(props.interviewer || null);
   // -- props :
   // name:String
   // interviewers:Array
   // interviewer:Number
   // onSave:Function
-  // onCancel:Function
+  // onCancel:Function 
+
+  function reset () {
+    setName("");
+    setInterviewer(null);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -19,21 +24,26 @@ export default function Form(props) {
     <form autoComplete="off">
       <input
         className="appointment__create-input text--semi-bold"
-        name="name"
+        // name="name"
         type="text"
         placeholder="Enter Student Name"
+        value={name}
+        // onChange={(event) => setName(event.target.value)}
+        onChange={setName}
+        
         /*
           This must be a controlled component
         */
       />
     </form>
-    <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
-    {/* <InterviewerList interviewers={props.interviewers} interviewer={props} setInterviewer={props}/> */}
+    {name}
+    {/* <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} /> */}
+    <InterviewerList interviewers={props.interviewers} interviewer={props.interviewer || interviewer} setInterviewer={props.onSave}/>
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger>Cancel</Button>
-      <Button confirm>Save</Button>
+      <Button danger onClick={reset} >Cancel</Button>
+      <Button confirm onClick={props.onSave} >Save</Button>
     </section>
   </section>
 </main>
