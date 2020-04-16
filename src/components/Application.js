0ab04fe6a4a52bd08365f2658ac17b8fc4ff5ac3
Hatem <hatem.deux@gmail.com>
2020-04-16 01:57:36 -0400

    fetches appointments data and  groups useEffect API requests under Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,8 +5,9 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "components/DayList.js";
 import Appointment from 'components/Appointment/index';
+import { getAppointmentsForDay } from '../helpers/selectors';
 
-const appointments = [
+/* const appointments = [
   {
     id: 1,
     time: "12pm",
@@ -43,26 +44,36 @@ const appointments = [
     id: 5,
     time: "4pm",
   },
-];
+]; 
+*/
 
 
 export default function Application(props) {
+  
   // const [day, setDay] = useState("Monday");
   // const [days, setDays] = useState([]);
+
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
   });
+
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+  
+  const appointments = getAppointmentsForDay(state, state.day);
 
   useEffect( ()=> {
-    axios.get(`/api/days`)
-    .then( (response) => {
-      setDays(response.data);
-    })
-  },[state.days])
+    Promise.all([
+      Promise.resolve(axios.get(`/api/days`)),
+      Promise.resolve(axios.get(`/api/appointments`))
+    ]).then((all) => {
+      console.log('all ===> ', all);
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      });
+    /* axios.get(`/api/days`).then( (response) => { setDays(response.data) })  */
+  },[])
 
   return (
     <main className="layout">
