105e2901627d8cf81144d22f1b009fe23c7b0fa7
Hatem <hatem.deux@gmail.com>
2020-04-16 00:16:37 -0400

    implements getAppointmentsForDay to pass  test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,26 @@
 export function getAppointmentsForDay(state, day) {
   
+  if (!state.days[0]) {return [] };
+  
+  const [selectedDay] =  state.days.filter(
+    singleDay => singleDay.name === day
+    );
+    
+  if (!selectedDay) {return []};
+
+  const selectedAppointments = selectedDay.appointments.map( 
+    singleAppointment => state.appointments[singleAppointment] 
+    );
+
+  return selectedAppointments;
 }
+
+
+/* 
+function selectUserByName(state, name) {
+  const filteredNames = state.users.filter(user => user.name === name);
+  return filteredNames;
+}
+
+2 keys: days / 
+*/
