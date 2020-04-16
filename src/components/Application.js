bdfef5b1b0ed09315198d7aa09c2e7985916b6e5
Hatem <hatem.deux@gmail.com>
2020-04-16 14:23:31 -0400

    implements helper getInterview() to get interview details from API + necessary changes

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "components/DayList.js";
 import Appointment from 'components/Appointment/index';
-import { getAppointmentsForDay } from '../helpers/selectors';
+import { getAppointmentsForDay, getInterview } from '../helpers/selectors';
 
 /* const appointments = [
   {
@@ -56,7 +56,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -67,13 +68,17 @@ export default function Application(props) {
   useEffect( ()=> {
     Promise.all([
       Promise.resolve(axios.get(`/api/days`)),
-      Promise.resolve(axios.get(`/api/appointments`))
+      Promise.resolve(axios.get(`/api/appointments`)),
+      Promise.resolve(axios.get(`/api/interviewers`))
     ]).then((all) => {
       console.log('all ===> ', all);
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
       });
     /* axios.get(`/api/days`).then( (response) => { setDays(response.data) })  */
   },[])
+
+  console.log('state.interviewers ===> ', state.interviewers)
+
 
   return (
     <main className="layout">
@@ -102,10 +107,12 @@ export default function Application(props) {
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
         {
           appointments.map(appointment => {
+            const interview = getInterview(state, appointment.interview);
             return (
               <Appointment 
               key={appointment.id} 
-              {...appointment} 
+              {...appointment}
+               interview={interview}
 
               />
 
