25800dd2967f799f23ba97ad6eb9aa73e7cd77bf
Hatem <hatem.deux@gmail.com>
2020-04-16 00:47:15 -0400

    removes individual useStates and combines them, implements reflected changes

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,15 +47,22 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect( ()=> {
     axios.get(`/api/days`)
     .then( (response) => {
       setDays(response.data);
     })
-  },[days])
+  },[state.days])
 
   return (
     <main className="layout">
@@ -69,8 +76,8 @@ export default function Application(props) {
           <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay/* day => console.log(day) */}
           />
           </nav>
