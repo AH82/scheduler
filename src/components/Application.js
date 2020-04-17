d21344524b08132fbe6ab6083d5865afd50a9a68
Hatem <hatem.deux@gmail.com>
2020-04-17 02:51:40 -0400

    imports & uses getInterviewersForDay() selector to get list of interviewers, passes down to appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "components/DayList.js";
 import Appointment from 'components/Appointment/index';
-import { getAppointmentsForDay, getInterview } from '../helpers/selectors';
+import { getAppointmentsForDay, getInterviewersForDay, getInterview } from '../helpers/selectors';
 
 /* const appointments = [
   {
@@ -64,6 +64,7 @@ export default function Application(props) {
   // const setDays = days => setState(prev => ({ ...prev, days }));
   
   const appointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
 
   useEffect( ()=> {
     Promise.all([
@@ -71,8 +72,13 @@ export default function Application(props) {
       Promise.resolve(axios.get(`/api/appointments`)),
       Promise.resolve(axios.get(`/api/interviewers`))
     ]).then((all) => {
-      console.log('all ===> ', all);
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+        console.log('all ===> ', all);
+        setState(prev => ({
+          ...prev, 
+          days: all[0].data, 
+          appointments: all[1].data, 
+          interviewers: all[2].data 
+          }));
       });
     /* axios.get(`/api/days`).then( (response) => { setDays(response.data) })  */
   },[])
@@ -113,6 +119,7 @@ export default function Application(props) {
               key={appointment.id} 
               {...appointment}
                interview={interview}
+               interviewers={interviewers}
 
               />
 
