be8d451f77ee636c292cbe99bdc55f977f21248b
Hatem <hatem.deux@gmail.com>
2020-04-16 21:44:54 -0400

    adds useVisualMode custom hook and its test. fully implemented and tested.

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+export default function useVisualMode (initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function (newMode, replace = false) {
+    const tempHistory = [...history]
+    if (replace) {
+      tempHistory.pop();
+      setHistory([...tempHistory]);
+    } 
+      setHistory([...tempHistory, newMode]);
+      return setMode(newMode) 
+    
+  }
+
+  const back = function () {
+
+    const tempHistory =  [...history];
+    tempHistory.pop();
+
+    setHistory(tempHistory);
+    const prevMode = tempHistory[tempHistory.length - 1];
+
+    if(tempHistory.length > 0) {
+      return setMode(prevMode);
+    }
+  }
+
+  return (
+    { mode, transition, back }
+  )
+};
