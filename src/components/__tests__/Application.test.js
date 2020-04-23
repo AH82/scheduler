8e74f293eb50d3bfd4d87e3362d0e26ff544cb2e
Hatem <hatem.deux@gmail.com>
2020-04-22 23:30:46 -0400

    adds tests to test booking interview, necessairy fixes implemented but failed. to be fixed later

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,82 @@
 import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
 import Application from "components/Application";
+
+import { 
+  render, 
+  cleanup, 
+  waitForElement, 
+  fireEvent, 
+  getByText, 
+  getByAltText, 
+  getByPlaceholderText, 
+  getAllByTestId,
+  queryByText,
+  prettyDOM
+} from "@testing-library/react";
+
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+
+describe("Application", () => {
+  // @AH82: While Compass ask to delete this test, I kept it for learning purposes. 
+  // it("renders without crashing", () => {
+  //   render(<Application />);
+  // });
+
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+
+  
+  it.only ("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { debug, container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    // console.log(prettyDOM(container));
+
+    const appointments = getAllByTestId(container, "appointment");
+    // console.log(prettyDOM(appointments));
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+    // console.log(prettyDOM(appointment));
+    
+    fireEvent.click(getByAltText(appointment, "Add"));
+    
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    
+    fireEvent.click(getByText(appointment, "Save"));
+    
+    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
+    
+    await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
+    // OR // waitForElementToBeRemoved
+    
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+      );
+      
+      // debug();
+      // console.log(prettyDOM(day));
+
+      // This does not apply to my code! code only reads it from the calculated back-end!
+      expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
+    });
+
+
 });
+
+// act(() => {
+//   /* fire events that update state */
+// });
+// /* assert on the output */
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -35,12 +35,82 @@ export default function useApplicationData (initial) {
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
   
   // -> re-gets days to re-render to update spots
-  function remainingSpots() {
-    axios.get(`/api/days`)
-    .then( (res) => {
-      setState(prev => ({...prev, days: res.data}));
-    });
+  // function remainingSpots() {
+  //   axios.get(`/api/days`)
+  //   .then( (res) => {
+  //     setState(prev => ({...prev, days: res.data}));
+  //   });
+  // };
+
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+ // try #3
+ 
+  function remainingSpots(id) { //This must me the appointment ID
+    // console.log("[remaining spots] id = ", id)
+    console.log("[remaining spots] state is = ", JSON.stringify(state, null, 2) )
+    let remainingSpots = 0;
+    let targetDay = null;
+    state.days.forEach((day, index) => {
+        day.appointments.forEach( (appointmentId, index, array) => {
+          // console.log("[remainingSpots]", array)
+          if(id === appointmentId) {
+            array.forEach( (appointmentIdAgain, index) => {
+              if(!state.appointments[appointmentIdAgain].interview) {
+                remainingSpots++;
+                targetDay = day.id
+                
+              }
+
+            } )
+          }
+        })
+    })
+    console.log("PPP",remainingSpots)
+    const days = [...state.days]
+    days[targetDay-1] = {...days[targetDay-1], spots: remainingSpots}
+    setState(prev => ({...prev, days}))
   };
+
+
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+  // Try #4
+/*
+  function remainingSpots(id, action) { //This must me the appointment ID
+    // console.log("[remaining spots] id = ", id)
+    // console.log("[remaining spots] state is = ", JSON.stringify(state, null, 2) )
+    let remainingSpots = 1;
+    let targetDay = null;
+    state.days.forEach((day, index) => {
+        day.appointments.forEach( (appointmentId, index, array) => {
+          // console.log("[remainingSpots]", array)
+          if(id === appointmentId) {
+            targetDay = day.id;
+            console.log("DAYDAY", targetDay, day.id)
+            if (action === "book") {
+              remainingSpots--;
+            } else if (action === "cancel") {
+              remainingSpots++
+             }
+          }
+        })
+    })
+    console.log("PPP",remainingSpots)
+    const days = [...state.days]
+    days[targetDay-1] = {...days[targetDay-1], spots: remainingSpots}
+    setState(prev => ({...prev, days}))
+  };
+*/
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+// try #2
+    // const day = {
+      
+    // }
+    // axios.get(`/api/days`)
+    // .then( (res) => {
+    //   console.log("remainingSpots function : ", res.data[id].appointments)
+    //   const spots = res.data[id].appointments.length
+    //   setState(prev => ({...prev, days.spots }));
+    // });
 
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -54,14 +124,22 @@ export default function useApplicationData (initial) {
       ...state.appointments,
       [id]: appointment
     };
+    console.log("bookInterview ===> ", JSON.stringify(appointments,null,2))
 
     return axios.put(`/api/appointments/${id}`, appointment )
     .then((response)=>{
       console.log('[bookInterview] axios put response = ', response);
-      setState({ ...state, appointments});
-      remainingSpots();
-
+      // ???? setState does not work !?!
+      setState({ ...state, appointments })
+      // .then(()=> console.log("KKK", state))
+      // setTimeout( () => {
+        //   console.log("YYY", state);
+        // }, 5000)
+        // setState(prev => ({ ...prev, appointments}));
+        remainingSpots(id, "book");
+        // console.log("[bookinterview] state is = ", JSON.stringify(state, null, 2) )
       })
+      // .then(()=> {console.log(">>>>", state)})
     };
   
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -78,9 +156,9 @@ export default function useApplicationData (initial) {
       
       return axios.delete(`/api/appointments/${id}`, appointment.interview)
       .then((response)=>{
-        console.log('[cancelInterview] axios put response = ', response);
+        console.log('[cancelInterview] axios delete response = ', response);
         setState({ ...state, appointments});
-        remainingSpots();
+        // remainingSpots(id);
         })
     };
 
@@ -91,7 +169,8 @@ export default function useApplicationData (initial) {
       state,
       setDay,
       bookInterview,
-      cancelInterview
+      cancelInterview,
+      // remainingSpots
     }
   );
 }
